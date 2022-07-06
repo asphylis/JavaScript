2d89b15beb3f1680aeccfdc89661d6de8bcf5b7e
asphylis <90554655+asphylis@users.noreply.github.com>
2022-07-06 15:25:28 +0530

    Create Variables.js
    Ways to declare variables and a little about their naming scheme

---
+++ Day 1/Variables.js
@@ -0,0 +1,24 @@
+/*
+Variables in JavaScript are normally declared using 
+    1. let
+    2. var
+    3. const
+    
+variable name must never start with numbers
+*/
+
+//Variables declared seperately
+let firstName = "Naruto";
+let lastName = "Uzumaki";
+let country = "Konoha";
+let position = "Hokage";
+
+//variable with boolean value
+let married = true;
+
+//variable with numerical value
+let numberOfKids = 2;
+
+
+//multiple variables declared in single row
+let name = "Hinata", surName = "Hyuga", role = "wife";
