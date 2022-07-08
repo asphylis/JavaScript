86db799b2a943e0bfc719f9ec127948b01b602cb
asphylis <90554655+asphylis@users.noreply.github.com>
2022-07-08 20:13:24 +0530

    Create Primitive_datatype.js

---
+++ Day 2/Primitive_datatype.js
@@ -0,0 +1,37 @@
+/*
+There are 7 primitive data types in JavaScript:
+  1. string
+  2. number
+  3. bigint
+  4. boolean
+  5. undefined
+  6. symbol
+  7. null
+All primitive datatypes are immutable
+*/
+
+//number
+let numOne = 3;
+let numTwo = 5;
+
+//String
+let js = 'JavaScript';
+let py = 'Python';
+
+//Boolean datatype
+//Mostly used in conditional operations
+let lightOn = true;
+let lightOff = false;
+
+if(lightOn){
+  console.log("It is bright");
+}else{
+  console.log("It is Dark"):
+}
+
+//undefined
+//variable having no value
+var x;
+
+//Symbol
+const sym = symbol('hello');
