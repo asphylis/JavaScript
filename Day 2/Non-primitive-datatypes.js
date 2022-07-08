3aaa217c7479432fa40332b4b0303b2aa5ad5efa
asphylis <90554655+asphylis@users.noreply.github.com>
2022-07-08 21:00:58 +0530

    Create Non-primitive-datatypes.js

---
+++ Day 2/Non-primitive-datatypes.js
@@ -0,0 +1,25 @@
+/*
+Non - Primitive datatypes are of 3 types :
+  1. Objects : represents instance through which we can access members
+  2. Arrays : Group of similar values(same data types)
+  3. RegExp : represent regular expressions
+*/
+
+//object
+const firstMate = {
+  firstName: "Roronoa",
+  lastName: "Zoro",
+  age: 21,
+  hairColor: "Green"
+};
+
+//another method
+//First create empty object and then add parameters
+const doctor = {};
+doctor.name = 'Tony Tony Chopper';
+doctor.fruit = 'Hito Hito No Mi';
+doctor.bounty = 100;
+
+
+//Arrays
+const newEmperors = ['Luffy', 'Buggy', 'Blackbeard', 'Shanks'];
